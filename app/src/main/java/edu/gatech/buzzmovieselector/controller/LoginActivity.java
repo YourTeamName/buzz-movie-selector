caaa3406f9acc796ba1b526944e00f4ed4bec593
Jason Wu <jasonwupilly1997@gmail.com>
2016-02-05 15:53:53 -0500

    create model and controller packages

--- app/src/main/java/edu/gatech/buzzmovieselector/controller/LoginActivity.java
+++ app/src/main/java/edu/gatech/buzzmovieselector/controller/LoginActivity.java
@@ -1,4 +1,4 @@
-package edu.gatech.buzzmovieselector;
+package edu.gatech.buzzmovieselector.controller;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -31,6 +31,8 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import edu.gatech.buzzmovieselector.R;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
