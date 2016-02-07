3932201c9e28b01415e53374e1f830925a11a355
Jason Wu <jasonwupilly1997@gmail.com>
2016-02-06 22:09:29 -0500

    simple welcome screen that checks if you are logged in already

--- app/src/main/java/edu/gatech/buzzmovieselector/controller/RegisterActivity.java
+++ app/src/main/java/edu/gatech/buzzmovieselector/controller/RegisterActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import edu.gatech.buzzmovieselector.R;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 }
---
+++ app/src/main/java/edu/gatech/buzzmovieselector/controller/WelcomeActivity.java
@@ -0,0 +1,43 @@
+package edu.gatech.buzzmovieselector.controller;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import edu.gatech.buzzmovieselector.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        if (checkLogin()) {
+            Toast.makeText(getApplicationContext(), "check login is true", Toast.LENGTH_SHORT).show();
+            Intent mainActivity = new Intent(this, BMSActivity.class);
+            startActivity(mainActivity);
+        }
+    }
+
+    private void allowAccess() {
+        // user is already logged in, so redirect to real menu
+    }
+
+    public void startLogin(View v) {
+        Toast.makeText(getApplicationContext(), "start login activity", Toast.LENGTH_SHORT).show();
+        Intent loginActivity = new Intent(this, LoginActivity.class);
+        startActivity(loginActivity);
+    }
+
+    public void startRegister(View v) {
+        Toast.makeText(getApplicationContext(), "start register activity", Toast.LENGTH_SHORT).show();
+        Intent registerActivity = new Intent(this, RegisterActivity.class);
+        startActivity(registerActivity);
+    }
+
+    private boolean checkLogin() {
+        return false;
+    }
+}
