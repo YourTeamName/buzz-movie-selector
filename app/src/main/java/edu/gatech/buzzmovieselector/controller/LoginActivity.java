c301004925f53352fa7a60caf6a2c2836b21a668
Jason Wu <jasonwupilly1997@gmail.com>
2016-02-06 21:55:25 -0500

    change the main activity to welcome screen and add preliminary UI

--- app/src/main/java/edu/gatech/buzzmovieselector/controller/LoginActivity.java
+++ app/src/main/java/edu/gatech/buzzmovieselector/controller/LoginActivity.java
@@ -1,36 +1,16 @@
 package edu.gatech.buzzmovieselector.controller;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import edu.gatech.buzzmovieselector.R;
 
@@ -38,7 +18,7 @@ public class LoginActivity extends Activity {
 
 
     // UI references.
-    private AutoCompleteTextView mEmailView;
+    private AutoCompleteTextView mUsernameView;
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
@@ -47,9 +27,7 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.username);
-
+        mUsernameView = (AutoCompleteTextView) findViewById(R.id.username);
         mPasswordView = (EditText) findViewById(R.id.password);
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -62,7 +40,7 @@ public class LoginActivity extends Activity {
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.login_button);
+        Button loginButton = (Button) findViewById(R.id.login_button);
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
@@ -79,6 +57,7 @@ public class LoginActivity extends Activity {
     private void attemptLogin() {
 
         Toast.makeText(getApplicationContext(), "login attempt", Toast.LENGTH_SHORT).show();
+        // TODO: Backend work goes on here
     }
 
 }
