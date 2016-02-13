809540a589998ff3b7f41b4bd5d5762766a36797
Jason Wu <jasonwupilly1997@gmail.com>
2016-02-13 18:37:02 -0500

    set value of firstNameText enabled to true

--- app/src/main/java/edu/gatech/buzzmovieselector/controller/ProfileActivity.java
+++ app/src/main/java/edu/gatech/buzzmovieselector/controller/ProfileActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -41,6 +42,48 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     /**
+     * Helper method for enable editable on an EditText
+     * @param e EditText view to enable
+     */
+    private void enableTextField(EditText e) {
+        e.setFocusable(true);
+        e.setClickable(true);
+        e.setFocusableInTouchMode(true);
+    }
+
+    /**
+     * Helper method for disabling editable on an EditText
+     * @param e EditText view to disable
+     */
+    private void disableTextField(EditText e) {
+        e.setFocusable(false);
+        e.setClickable(false);
+        e.setFocusableInTouchMode(false);
+    }
+
+    /**
+     * disables all elements in the profile form
+     */
+    private void disableForm() {
+        disableTextField(firstNameText);
+        disableTextField(lastNameText);
+        degreeSpinner.setClickable(false);
+        degreeSpinner.setFocusableInTouchMode(false);
+        disableTextField(emailText);
+    }
+
+    /**
+     * enables all elements in the profile form
+     */
+    private void enableForm() {
+        enableTextField(firstNameText);
+        enableTextField(lastNameText);
+        degreeSpinner.setClickable(true);
+        degreeSpinner.setFocusableInTouchMode(true);
+        enableTextField(emailText);
+    }
+
+    /**
      * Retrieves user parameter from passed bundle and uses it to initialize form
      */
     private void initializeForm() {
@@ -57,8 +100,18 @@ public class ProfileActivity extends AppCompatActivity {
             profileUser = um.findUserById(bundledUserName);
         }
         userNameLabel.setText(profileUser.getUsername());
+        disableForm();
+        cancelFocus();
         populateSpinner();
         populateFields();
+    }
+
+    /**
+     * Cancels focus from all form elements
+     */
+    private void cancelFocus() {
+        RelativeLayout formLayout = (RelativeLayout) findViewById(R.id.profileLayout);
+        formLayout.requestFocus();
     }
 
     /**
