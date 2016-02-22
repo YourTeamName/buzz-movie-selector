485c25e425db3b2e53e0d3cf59a1531574ddbf96
Jason Wu <jasonwupilly1997@gmail.com>
2016-02-21 20:37:00 -0500

    remove unused imports in bmsactivity

--- app/src/main/java/edu/gatech/buzzmovieselector/controller/BMSActivity.java
+++ app/src/main/java/edu/gatech/buzzmovieselector/controller/BMSActivity.java
@@ -20,15 +20,11 @@ import android.widget.ListView;
 import edu.gatech.buzzmovieselector.R;
 import edu.gatech.buzzmovieselector.biz.api.ApiCall;
 import edu.gatech.buzzmovieselector.biz.api.ApiCallback;
-import edu.gatech.buzzmovieselector.biz.api.ApiReceiver;
 import edu.gatech.buzzmovieselector.biz.api.impl.rt.RTInvoker;
 import edu.gatech.buzzmovieselector.biz.api.impl.rt.command.RTCommandFactory;
 import edu.gatech.buzzmovieselector.biz.api.impl.rt.receiver.RTMovieListReceiver;
 import edu.gatech.buzzmovieselector.entity.Movie;
 import edu.gatech.buzzmovieselector.service.SessionState;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 
