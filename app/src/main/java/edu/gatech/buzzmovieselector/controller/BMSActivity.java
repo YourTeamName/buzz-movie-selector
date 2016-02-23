ef820a7c7e31539b0c47caeec94fee101fb5d3f1
zhangj150 <zhangj@gatech.edu>
2016-02-22 21:59:59 -0500

    i made some progress but am still having trouble making it all work, can someone look over it for me? thanks

--- app/src/main/java/edu/gatech/buzzmovieselector/controller/BMSActivity.java
+++ app/src/main/java/edu/gatech/buzzmovieselector/controller/BMSActivity.java
@@ -36,6 +36,7 @@ import java.util.ArrayList;
 public class BMSActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private SearchView s;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,7 +53,7 @@ public class BMSActivity extends AppCompatActivity
             }
         });
 
-        SearchView s = (SearchView) findViewById(R.id.searchView);
+        s = (SearchView) findViewById(R.id.searchView);
         s.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
@@ -89,7 +90,7 @@ public class BMSActivity extends AppCompatActivity
         ListView recentDVDs = (ListView) findViewById(R.id.recentDVDsList);
         recentDVDs.setAdapter(listAdapter);
         RTInvoker rti = new RTInvoker();
-        rti.executeCall(new ApiCall(RTCommandFactory.getMovieSearchCommand("Django"), new ApiCallback<RTMovieListReceiver>() {
+        rti.executeCall(new ApiCall(RTCommandFactory.getRecentMoviesCommand(), new ApiCallback<RTMovieListReceiver>() {
             @Override
             public void onReceive(RTMovieListReceiver receiver) {
                 for (Movie m : receiver.getEntity()) {
@@ -109,6 +110,7 @@ public class BMSActivity extends AppCompatActivity
      */
     private void startResultsActivity() {
         Intent intent = new Intent(this, SearchResultsActivity.class);
+        intent.putExtra(SearchResultsActivity.CURRENT_QUERY, s.getQuery().toString());
         startActivity(intent);
     }
 
--- app/src/main/java/edu/gatech/buzzmovieselector/controller/SearchResultsActivity.java
+++ app/src/main/java/edu/gatech/buzzmovieselector/controller/SearchResultsActivity.java
@@ -1,15 +1,52 @@
 package edu.gatech.buzzmovieselector.controller;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import edu.gatech.buzzmovieselector.R;
+import edu.gatech.buzzmovieselector.biz.api.ApiCall;
+import edu.gatech.buzzmovieselector.biz.api.ApiCallback;
+import edu.gatech.buzzmovieselector.biz.api.impl.rt.RTInvoker;
+import edu.gatech.buzzmovieselector.biz.api.impl.rt.command.RTCommandFactory;
+import edu.gatech.buzzmovieselector.biz.api.impl.rt.receiver.RTMovieListReceiver;
+import edu.gatech.buzzmovieselector.entity.Movie;
+
+import java.util.ArrayList;
+import android.util.Log;
 
 public class SearchResultsActivity extends AppCompatActivity {
+    public static final String CURRENT_QUERY = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
         //Log.d("MY", "search activity triggered");
+        showResults();
+    }
+
+    private void showResults() {
+        final ArrayList<String> dvdList = new ArrayList<>();
+        // TODO: make a custom list adapter to work with POJOs
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dvdList);
+        ListView matchingDVDs = (ListView) findViewById(R.id.searchResults);
+        matchingDVDs.setAdapter(listAdapter);
+        RTInvoker rti = new RTInvoker();
+        String bundledSearchQuery = getIntent().getExtras().getString(CURRENT_QUERY, null);
+        rti.executeCall(new ApiCall(RTCommandFactory.getMovieSearchCommand(bundledSearchQuery), new ApiCallback<RTMovieListReceiver>() {
+            @Override
+            public void onReceive(RTMovieListReceiver receiver) {
+                for (Movie m : receiver.getEntity()) {
+                    dvdList.add(m.toString());
+                }
+            }
+        }));
+
+        for (String sd : dvdList) {
+            Log.v("BMSActivity", sd);
+        }
     }
 }
