3d87ded438903a2d3aaa37ac0672b2a674016563
Nikhil Ramanan <nramanan6@gatech.edu>
2016-02-22 22:57:06 -0500

    Fixed merge

--- app/src/main/java/edu/gatech/buzzmovieselector/controller/SearchResultsActivity.java
+++ app/src/main/java/edu/gatech/buzzmovieselector/controller/SearchResultsActivity.java
@@ -16,7 +16,6 @@ import edu.gatech.buzzmovieselector.biz.api.ApiCall;
 import edu.gatech.buzzmovieselector.biz.api.ApiCallback;
 import edu.gatech.buzzmovieselector.biz.api.impl.rt.RTInvoker;
 import edu.gatech.buzzmovieselector.biz.api.impl.rt.command.RTCommandFactory;
-import edu.gatech.buzzmovieselector.biz.api.impl.rt.receiver.RTMovieListReceiver;
 import edu.gatech.buzzmovieselector.entity.Movie;
 
 /**
@@ -24,7 +23,6 @@ import edu.gatech.buzzmovieselector.entity.Movie;
  * Intent from BMSActivity contains the search text
  */
 public class SearchResultsActivity extends AppCompatActivity {
-    public static final String CURRENT_QUERY = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,29 +30,6 @@ public class SearchResultsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search_results);
         displayResults();
         //Log.d("MY", "search activity triggered");
-        showResults();
-    }
-
-    private void showResults() {
-        final ArrayList<String> dvdList = new ArrayList<>();
-        // TODO: make a custom list adapter to work with POJOs
-        ArrayAdapter<String> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dvdList);
-        ListView matchingDVDs = (ListView) findViewById(R.id.searchResults);
-        matchingDVDs.setAdapter(listAdapter);
-        RTInvoker rti = new RTInvoker();
-        String bundledSearchQuery = getIntent().getExtras().getString(CURRENT_QUERY, null);
-        rti.executeCall(new ApiCall(RTCommandFactory.getMovieSearchCommand(bundledSearchQuery), new ApiCallback<RTMovieListReceiver>() {
-            @Override
-            public void onReceive(RTMovieListReceiver receiver) {
-                for (Movie m : receiver.getEntity()) {
-                    dvdList.add(m.toString());
-                }
-            }
-        }));
-
-        for (String sd : dvdList) {
-            Log.v("BMSActivity", sd);
-        }
     }
 
     /**
@@ -75,7 +50,7 @@ public class SearchResultsActivity extends AppCompatActivity {
             }
         }));
         ArrayAdapter<String> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dvdList);
-        ListView recentDVDs = (ListView) findViewById(R.id.listView);
+        ListView recentDVDs = (ListView) findViewById(R.id.searchResults);
         recentDVDs.setAdapter(listAdapter);
 
         for (String sd : dvdList) {
