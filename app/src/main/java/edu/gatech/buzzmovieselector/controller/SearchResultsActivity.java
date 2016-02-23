a1b56dd613ae69192aeb2bc3e9356cd03c930063
Jianing Yang <jed970610@gmail.com>
2016-02-23 14:10:39 -0500

    change to use runOnUIThread, document code

--- app/src/main/java/edu/gatech/buzzmovieselector/controller/SearchResultsActivity.java
+++ app/src/main/java/edu/gatech/buzzmovieselector/controller/SearchResultsActivity.java
@@ -1,23 +1,20 @@
 package edu.gatech.buzzmovieselector.controller;
 
-import edu.gatech.buzzmovieselector.biz.api.impl.rt.receiver.RTMovieListReceiver;
-
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import java.util.ArrayList;
-
 import edu.gatech.buzzmovieselector.R;
 import edu.gatech.buzzmovieselector.biz.api.ApiCall;
 import edu.gatech.buzzmovieselector.biz.api.ApiCallback;
 import edu.gatech.buzzmovieselector.biz.api.impl.rt.RTInvoker;
 import edu.gatech.buzzmovieselector.biz.api.impl.rt.command.RTCommandFactory;
+import edu.gatech.buzzmovieselector.biz.api.impl.rt.receiver
+        .RTMovieListReceiver;
 import edu.gatech.buzzmovieselector.entity.Movie;
+
+import java.util.ArrayList;
 
 /**
  * Search results activity that displays the results of a search
@@ -25,11 +22,11 @@ import edu.gatech.buzzmovieselector.entity.Movie;
  */
 public class SearchResultsActivity extends AppCompatActivity {
 
-    private static Handler handler;
+    public static final String SEARCH_KEYWORD = "searchKeyword";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        handler = new Handler();
         setContentView(R.layout.activity_search_results);
         displayResults();
         //Log.d("MY", "search activity triggered");
@@ -40,38 +37,34 @@ public class SearchResultsActivity extends AppCompatActivity {
      */
     private void displayResults() {
         final ArrayList<String> dvdList = new ArrayList<>();
-        // TODO: make a custom list adapter to work with POJOs
-        final ArrayAdapter<String> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dvdList);
-        final ListView recentDVDs = (ListView) findViewById(R.id.searchResults);
+
+        // Create customized adapter for list view
+        final ArrayAdapter<String> listAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, dvdList);
+        ListView recentDVDs = (ListView) findViewById(R.id.searchResults);
         recentDVDs.setAdapter(listAdapter);
+
+        // Call RT api to search for movies
         RTInvoker rti = new RTInvoker();
         Intent intent = getIntent();
-        String search = intent.getStringExtra("search"); // Get search query
-        rti.executeCall(new ApiCall(RTCommandFactory.getMovieSearchCommand(search), new ApiCallback<RTMovieListReceiver>() {
+        // Get search query
+        String search = intent.getStringExtra(SEARCH_KEYWORD);
+        // Excute the command and refresh list view
+        rti.executeCall(new ApiCall(RTCommandFactory.getMovieSearchCommand
+                (search), new ApiCallback<RTMovieListReceiver>() {
             @Override
             public void onReceive(RTMovieListReceiver receiver) {
                 for (Movie m : receiver.getEntity()) {
                     dvdList.add(m.toString());
                 }
-                SearchResultsActivity.getHandler().post(new Runnable() {
+                // NotifyDataSetChanged() must be called on the UI thread
+                runOnUiThread(new Runnable() {
                     public void run() {
                         listAdapter.notifyDataSetChanged();
                     }
                 });
             }
         }));
-
-        for (String sd : dvdList) {
-            Log.v("BMSActivity", sd);
-        }
-    }
-
-    /**
-     * Accessor method for the activity's handler
-     * @return The handler for the activity on which to execute methods
-     */
-    private static Handler getHandler() {
-        return handler;
     }
 
 }
