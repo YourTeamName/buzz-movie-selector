02dc3e7d769dfee659ce980eaa2ea105e28ec89c
Jason Wu <jasonwupilly1997@gmail.com>
2016-02-23 20:25:09 -0500

    add fragments to a tabbed layout

--- app/src/main/java/edu/gatech/buzzmovieselector/controller/activity/BMSActivity.java
+++ app/src/main/java/edu/gatech/buzzmovieselector/controller/activity/BMSActivity.java
@@ -1,11 +1,15 @@
 package edu.gatech.buzzmovieselector.controller.activity;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -14,21 +18,12 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.SearchView;
 import edu.gatech.buzzmovieselector.R;
-import edu.gatech.buzzmovieselector.biz.api.ApiCall;
-import edu.gatech.buzzmovieselector.biz.api.ApiCallback;
-import edu.gatech.buzzmovieselector.biz.api.impl.rt.RTInvoker;
-import edu.gatech.buzzmovieselector.biz.api.impl.rt.command.RTCommandFactory;
-import edu.gatech.buzzmovieselector.biz.api.impl.rt.receiver
-        .RTMovieListReceiver;
-import edu.gatech.buzzmovieselector.controller.util.MovieAdapter;
-import edu.gatech.buzzmovieselector.entity.Movie;
+import edu.gatech.buzzmovieselector.controller.fragment.MovieSearchFragment;
+import edu.gatech.buzzmovieselector.controller.fragment.RecentDVDsFragment;
+import edu.gatech.buzzmovieselector.controller.fragment.RecentMoviesFragment;
+import edu.gatech.buzzmovieselector.controller.util.ViewPagerAdapter;
 import edu.gatech.buzzmovieselector.service.SessionState;
-
-import java.util.ArrayList;
 
 /**
  * BMSActivity is the main screen for the app. It is only displayed to users
@@ -37,7 +32,9 @@ import java.util.ArrayList;
 public class BMSActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private SearchView searchView;
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,21 +53,6 @@ public class BMSActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
-
-        searchView = (SearchView) findViewById(R.id.searchView);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String s) {
-                startResultsActivity(s);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String s) {
-                return false;
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R
@@ -81,6 +63,17 @@ public class BMSActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id
                 .nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new RecentDVDsFragment(), "DVDs");
+        adapter.addFragment(new RecentMoviesFragment(), "Movies");
+        adapter.addFragment(new MovieSearchFragment(), "Search");
+        viewPager.setAdapter(adapter);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
         if (SessionState.getInstance().isLoggedIn()) {
             Log.v("BMS", "we are logged in");
         } else {
@@ -88,52 +81,6 @@ public class BMSActivity extends AppCompatActivity
             // we should not be here unless we are logged in
             finish();
         }
-
-        /*
-        // for test purposes only
-        final ArrayList<String> dvdList = new ArrayList<>();
-        // Make a custom list adapter for list view
-        final ArrayAdapter<String> listAdapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, dvdList);
-        ListView recentDVDs = (ListView) findViewById(R.id.recentDVDsList);
-        recentDVDs.setAdapter(listAdapter);
-
-        // Call RT api to retrieve recent movies
-        RTInvoker rti = new RTInvoker();
-        rti.executeCall(new ApiCall(RTCommandFactory.getRecentMoviesCommand()
-                , new ApiCallback<RTMovieListReceiver>() {
-            @Override
-            public void onReceive(RTMovieListReceiver receiver) {
-                for (Movie m : receiver.getEntity()) {
-                    dvdList.add(m.toString());
-                }
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        listAdapter.notifyDataSetChanged();
-                    }
-                });
-            }
-        }));
-        */
-        final ArrayList<Movie> mList = new ArrayList<>();
-        ListView recentDVDs = (ListView) findViewById(R.id.recentDVDsList);
-        final MovieAdapter movAdapter = new MovieAdapter(this, mList);
-        recentDVDs.setAdapter(movAdapter);
-        RTInvoker rti = new RTInvoker();
-        rti.executeCall(new ApiCall(RTCommandFactory.getRecentMoviesCommand()
-                , new ApiCallback<RTMovieListReceiver>() {
-            @Override
-            public void onReceive(RTMovieListReceiver receiver) {
-                for (Movie m : receiver.getEntity()) {
-                    mList.add(m);
-                }
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        movAdapter.notifyDataSetChanged();
-                    }
-                });
-            }
-        }));
     }
 
     /**
--- app/src/main/java/edu/gatech/buzzmovieselector/controller/fragment/MovieSearchFragment.java
+++ app/src/main/java/edu/gatech/buzzmovieselector/controller/fragment/MovieSearchFragment.java
@@ -3,7 +3,7 @@ package edu.gatech.buzzmovieselector.controller.fragment;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,30 +11,82 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.SearchView;
 import edu.gatech.buzzmovieselector.R;
+import edu.gatech.buzzmovieselector.biz.api.ApiCall;
+import edu.gatech.buzzmovieselector.biz.api.ApiCallback;
+import edu.gatech.buzzmovieselector.biz.api.impl.rt.RTInvoker;
+import edu.gatech.buzzmovieselector.biz.api.impl.rt.command.RTCommandFactory;
+import edu.gatech.buzzmovieselector.biz.api.impl.rt.receiver.RTMovieListReceiver;
+import edu.gatech.buzzmovieselector.controller.util.MovieAdapter;
+import edu.gatech.buzzmovieselector.entity.Movie;
+
+import java.util.ArrayList;
 
 public class MovieSearchFragment extends Fragment {
 
     private ListView movieResults;
     private SearchView searchBar;
+    private Activity hostActivity;
+
+    private String searchQuery = "";
+
+    private View fragView;
 
     public MovieSearchFragment() {
-
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        movieResults = (ListView) getView().findViewById(R.id.movieSearchList);
-        searchBar = (SearchView) getView().findViewById(R.id.searchView);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_movie_search, container, false);
+        fragView = inflater.inflate(R.layout.fragment_movie_search, container, false);
+        movieResults = (ListView) fragView.findViewById(R.id.movieSearchList);
+        searchBar = (SearchView) fragView.findViewById(R.id.searchView);
+        searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                searchQuery = s;
+                refreshResults(fragView);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        return fragView;
     }
 
     @Override
     public void onAttach(Context context) {
-        Activity hostActivity = getActivity();
+        super.onAttach(context);
+        hostActivity = getActivity();
+    }
+
+    public void refreshResults(View v) {
+        final ArrayList<Movie> mList = new ArrayList<>();
+        final MovieAdapter movAdapter = new MovieAdapter(hostActivity, mList);
+        movieResults.setAdapter(movAdapter);
+        RTInvoker rti = new RTInvoker();
+        rti.executeCall(new ApiCall(RTCommandFactory.getMovieSearchCommand(searchQuery)
+                , new ApiCallback<RTMovieListReceiver>() {
+            @Override
+            public void onReceive(RTMovieListReceiver receiver) {
+                for (Movie m : receiver.getEntity()) {
+                    mList.add(m);
+                }
+                if (hostActivity == null) {
+                    return;
+                }
+                hostActivity.runOnUiThread(new Runnable() {
+                    public void run() {
+                        movAdapter.notifyDataSetChanged();
+                    }
+                });
+            }
+        }));
     }
 }
