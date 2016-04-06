2c193d3412f3b4158a58ef44221cd2a0d3b20e80
Jeremy <jereyzhang12@gmail.com>
2016-04-06 15:24:35 -0400

    fix

--- app/src/test/java/edu/gatech/buzzmovieselector/JingtianZhangJUnitTest.java
+++ app/src/test/java/edu/gatech/buzzmovieselector/JingtianZhangJUnitTest.java
@@ -9,36 +9,36 @@ public class JingtianZhangJUnitTest {
     @Test
     public void checkConstructorIgnoreCaseAdmin() {
         User a = new User("a", "1234", "ADMIN");
-        assertEquals(a.getUserStatus, User.UserStatus.ADMIN);
+        assertEquals(a.getUserStatus(), User.UserStatus.ADMIN);
 
         User b = new User("b", "1234", "aDMIN");
-        assertEquals(b.getUserStatus, User.UserStatus.ADMIN);
+        assertEquals(b.getUserStatus(), User.UserStatus.ADMIN);
     }
 
     @Test
     public void checkConstructorIgnoreCaseUser() {
         User c = new User("c", "1234", "USER");
-        assertEquals(c.getUserStatus, User.UserStatus.USER);
+        assertEquals(c.getUserStatus(), User.UserStatus.USER);
 
         User d = new User("d", "1234", "uSer");
-        assertEquals(d.getUserStatus, User.UserStatus.USER);
+        assertEquals(d.getUserStatus(), User.UserStatus.USER);
     }
 
     @Test
     public void checkConstructorIgnoreCaseBanned() {
         User e = new User("e", "1234", "Banned");
-        assertEquals(e.getUserStatus, User.UserStatus.BANNED);
+        assertEquals(e.getUserStatus(), User.UserStatus.BANNED);
 
         User f = new User("f", "1234", "bAnned");
-        assertEquals(f.getUserStatus, User.UserStatus.BANNED);
+        assertEquals(f.getUserStatus(), User.UserStatus.BANNED);
     }
 
     @Test
     public void checkConstructorIgnoreCaseLocked() {
         User g = new User("g", "1234", "LOCKED");
-        assertEquals(g.getUserStatus, User.UserStatus.LOCKED);
+        assertEquals(g.getUserStatus(), User.UserStatus.LOCKED);
 
         User h = new User("h", "1234", "Locked");
-        assertEquals(b.getUserStatus, User.UserStatus.LOCKED);
+        assertEquals(b.getUserStatus(), User.UserStatus.LOCKED);
     }
 }
