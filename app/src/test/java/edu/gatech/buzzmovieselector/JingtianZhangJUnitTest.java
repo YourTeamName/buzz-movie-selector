d042a2d90b5d1fe94a9c16c4412a6d0bf73aaa67
Jeremy <jereyzhang12@gmail.com>
2016-04-06 15:02:25 -0400

    fix

--- app/src/test/java/edu/gatech/buzzmovieselector/JingtianZhangJUnitTest.java
+++ app/src/test/java/edu/gatech/buzzmovieselector/JingtianZhangJUnitTest.java
@@ -7,8 +7,7 @@ import edu.gatech.buzzmovieselector.entity.User;
 
 public class JingtianZhangJUnitTest {
     @Test
-    public void checkConstructorIgnoreCase() {
-        //User a = new User("a", "1234", "admin");
+    public void checkConstructorIgnoreCaseAdmin() {
         User a = new User("a", "1234", "ADMIN");
         assertEquals(a.userStatus, UserStatus.ADMIN);
 
@@ -16,4 +15,30 @@ public class JingtianZhangJUnitTest {
         assertEquals(b.userStatus, UserStatus.ADMIN);
     }
 
+    @Test
+    public void checkConstructorIgnoreCaseUser() {
+        User c = new User("c", "1234", "USER");
+        assertEquals(c.userStatus, UserStatus.USER);
+
+        User d = new User("d", "1234", "uSer");
+        assertEquals(d.userStatus, UserStatus.USER);
+    }
+
+    @Test
+    public void checkConstructorIgnoreCaseBanned() {
+        User e = new User("e", "1234", "Banned");
+        assertEquals(e.userStatus, UserStatus.BANNED);
+
+        User f = new User("f", "1234", "bAnned");
+        assertEquals(f.userStatus, UserStatus.BANNED);
+    }
+
+    @Test
+    public void checkConstructorIgnoreCaseAdmin() {
+        User g = new User("g", "1234", "LOCKED");
+        assertEquals(g.userStatus, UserStatus.LOCKED);
+
+        User h = new User("h", "1234", "Locked");
+        assertEquals(b.userStatus, UserStatus.LOCKED);
+    }
 }
