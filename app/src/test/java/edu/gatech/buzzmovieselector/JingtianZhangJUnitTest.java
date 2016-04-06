410d6ea29ccd84a0ba7e2b13dd493e72b5f45ecc
Jeremy <jereyzhang12@gmail.com>
2016-04-06 15:22:00 -0400

    fix

--- app/src/test/java/edu/gatech/buzzmovieselector/JingtianZhangJUnitTest.java
+++ app/src/test/java/edu/gatech/buzzmovieselector/JingtianZhangJUnitTest.java
@@ -9,36 +9,36 @@ public class JingtianZhangJUnitTest {
     @Test
     public void checkConstructorIgnoreCaseAdmin() {
         User a = new User("a", "1234", "ADMIN");
-        assertEquals(a.userStatus, UserStatus.ADMIN);
+        assertEquals(a.UserStatus, UserStatus.ADMIN);
 
         User b = new User("b", "1234", "aDMIN");
-        assertEquals(b.userStatus, UserStatus.ADMIN);
+        assertEquals(b.UserStatus, UserStatus.ADMIN);
     }
 
     @Test
     public void checkConstructorIgnoreCaseUser() {
         User c = new User("c", "1234", "USER");
-        assertEquals(c.userStatus, UserStatus.USER);
+        assertEquals(c.UserStatus, UserStatus.USER);
 
         User d = new User("d", "1234", "uSer");
-        assertEquals(d.userStatus, UserStatus.USER);
+        assertEquals(d.UserStatus, UserStatus.USER);
     }
 
     @Test
     public void checkConstructorIgnoreCaseBanned() {
         User e = new User("e", "1234", "Banned");
-        assertEquals(e.userStatus, UserStatus.BANNED);
+        assertEquals(e.UserStatus, UserStatus.BANNED);
 
         User f = new User("f", "1234", "bAnned");
-        assertEquals(f.userStatus, UserStatus.BANNED);
+        assertEquals(f.UserStatus, UserStatus.BANNED);
     }
 
     @Test
     public void checkConstructorIgnoreCaseLocked() {
         User g = new User("g", "1234", "LOCKED");
-        assertEquals(g.userStatus, UserStatus.LOCKED);
+        assertEquals(g.UserStatus, UserStatus.LOCKED);
 
         User h = new User("h", "1234", "Locked");
-        assertEquals(b.userStatus, UserStatus.LOCKED);
+        assertEquals(b.UserStatus, UserStatus.LOCKED);
     }
 }
